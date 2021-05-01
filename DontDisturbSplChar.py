541902989cc04bdfbb03f2a8761e3e12c24859f7
MadhumithaS2001 <52739023+MadhumithaS2001@users.noreply.github.com>
2021-05-01 10:40:53 +0530

    Create DontDisturbSplChar.py

---
+++ DontDisturbSplChar.py
@@ -0,0 +1,40 @@
+'''
+Program Statement:
+Reverse a given string keeping its special character at the same place
+Input Format:
+string
+Output Format:
+reverse the string
+
+
+import re
+s=input().strip()
+
+r=re.findall("[a-zA-Z]",s)
+r.reverse()
+
+out=""
+j=0
+for i in s:
+  if(i not in "qwertyuiopasdfghjklzxcvbnmQWERTAYUIOPSDZFXCGVHBJNKML"):
+    out+=i
+  else:
+    out+=r[j]
+    j+=1
+print(out.strip())
+
+
+
+'''
+
+
+import re
+st=input().strip()
+r=re.findall("[a-zA-Z]",st)
+r.reverse()
+#sp=r'[@_!\#$%^&*()<>?/|}{~:]'
+al='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for i in range(len(st)):
+    if(st[i] not in al):
+        r.insert(i,st[i])
+print(''.join(r))
